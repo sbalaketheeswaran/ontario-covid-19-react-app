0aa86174cb10175a39d8ef1543296ae010793bfc
sbalaketheeswaran <sai.balaketheeswaran@gmail.com>
2020-10-01 06:13:48 -0400

    Remove unused import for styles

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Grid from "@material-ui/core/Grid/Grid";
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-});
 
 const rowHeaderCells = [
   { id: 'PublicHealthUnit', numeric: false, label: 'Public Health ID' },
@@ -24,45 +18,46 @@ const rowHeaderCells = [
 ];
 
 export default function SimpleTable(props) {
-  const classes = useStyles();
   console.log(props.data)
   if(props.data){
     return (
-      <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-            {rowHeaderCells.map((rowHeadCell) => ( 
-            <TableCell
-            key={rowHeadCell.id}
-            align={rowHeadCell.numeric ? 'right' : 'left'}
-          >
-              <TableSortLabel
-              active={props.columnToSort  === rowHeadCell.id}
-              direction={props.columnToSort  === rowHeadCell.id ? props.sortDirection : 'asc'}
-              onClick={() => props.handleSort(rowHeadCell.id)}
-            >
-              {rowHeadCell.label}
-            </TableSortLabel>
-          </TableCell>
-        ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {props.data.map((row) => ( 
-              <TableRow key={row.PublicHealthUnit}>
-                <TableCell align="left" component="th" scope="row">
-                {row.PublicHealthUnit}
-                </TableCell>
-                <TableCell align="right">{row.Total}</TableCell>
-                <TableCell align="right">{row.Recovered}</TableCell>
-                <TableCell align="right">{row.NotResolved}</TableCell>
-                <TableCell align="right">{row.Fatal}</TableCell>
-              </TableRow>
+      <Grid container justify={"center"}>
+        <Grid item xs={12} md={10} style={{ padding: "8px" }}>
+          <Paper style={{ overflowX: "auto" }}>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                {rowHeaderCells.map((rowHeadCell) => ( 
+                <TableCell
+                key={rowHeadCell.id}
+                align={rowHeadCell.numeric ? 'right' : 'left'}>
+                  <TableSortLabel
+                  active={props.columnToSort  === rowHeadCell.id}
+                  direction={props.columnToSort  === rowHeadCell.id ? props.sortDirection : 'asc'}
+                  onClick={() => props.handleSort(rowHeadCell.id)}>
+                  {rowHeadCell.label}
+                </TableSortLabel>
+              </TableCell>
             ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {props.data.map((row) => ( 
+                  <TableRow key={row.PublicHealthUnit}>
+                    <TableCell align="left" component="th" scope="row">
+                    {row.PublicHealthUnit}
+                    </TableCell>
+                    <TableCell align="right">{row.Total}</TableCell>
+                    <TableCell align="right">{row.Recovered}</TableCell>
+                    <TableCell align="right">{row.NotResolved}</TableCell>
+                    <TableCell align="right">{row.Fatal}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </Paper>
+        </Grid>
+      </Grid>
     );
   }
   return "Currently Loading..."
