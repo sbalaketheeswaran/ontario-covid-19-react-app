8db4e5f3327571f9d784d04a3c5a5bf5dadb049c
sbalaketheeswaran <sai.balaketheeswaran@gmail.com>
2020-11-24 06:06:42 -0500

    Updates

---
+++ precache-manifest.be21784c15d5ef0cc8cc1abad7db34df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b8e1cc57952cf1f0742a4da94c351e",
+    "url": "/sbalaketheeswaran/ontario-covid-19-react-app/index.html"
+  },
+  {
+    "revision": "be0c207e1cd46669492a",
+    "url": "/sbalaketheeswaran/ontario-covid-19-react-app/static/css/main.d0e80729.chunk.css"
+  },
+  {
+    "revision": "9c9996a7b220154593f9",
+    "url": "/sbalaketheeswaran/ontario-covid-19-react-app/static/js/2.f62590c3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sbalaketheeswaran/ontario-covid-19-react-app/static/js/2.f62590c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be0c207e1cd46669492a",
+    "url": "/sbalaketheeswaran/ontario-covid-19-react-app/static/js/main.97637dfd.chunk.js"
+  },
+  {
+    "revision": "10bb07bcf18a5690c83a",
+    "url": "/sbalaketheeswaran/ontario-covid-19-react-app/static/js/runtime-main.66e28366.js"
+  }
+]);
